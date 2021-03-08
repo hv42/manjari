4093c00f6c2f826fb5428631d36361197387cbf4
Haritha Veeraghanta <53350786+hv42@users.noreply.github.com>
2021-03-07 20:13:51 -0800

    add grayscale with cv2, image

--- intro1.py
+++ intro1.py
@@ -1,13 +1,33 @@
 import pytesseract
-import PIL
-from PIL import Image
-import os
-import re
+# import PIL
+# from PIL import Image, ImageEnhance, ImageOps
+# import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-src = os.path.join('C:\\Users\\harithavhs\\Documents\\github\\indic\\quote.jpeg')
-img = Image.open('C:\\Users\\harithavhs\\Documents\\github\\indic\\quote.jpeg')
-print(img)
-result = pytesseract.image_to_string(img)
-print(result.strip().split('\n'))
+# result = pytesseract.image_to_string(img)
+# print(result.strip().split('\n'))
+
+# Using Image: Open the original image and apply grayscale image with 'L' mode.
+# img = Image.open('C:\\Users\\harithavhs\\Documents\\github\\indic\\landscape.jpg').convert('L')
+# img.show()
+
+# enhancer1 = ImageEnhance.Sharpness(img)
+# enhancer2 = ImageEnhance.Contrast(img)
+
+# Using cv2 module: Open image and convert image to grayscale
+# img = cv2.imread('C:\\Users\\harithavhs\\Documents\\github\\indic\\landscape.jpg')
+
+# grayimg = img
+# height, width, channels = img.shape
+
+# for h in range(height):
+#     for w in range(width):
+#         grayimg[h,w] = 0.3 * img[h,w][0] + 0.59 * img[h,w][1] + 0.11 * img[h,w][2]
+
+#cv2.imshow('source image', img)
+# cv2.imshow('gray image', grayimg)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
